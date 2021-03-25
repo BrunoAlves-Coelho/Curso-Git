044f738a2b4f0cbdae743de2c95f0c1db28d4313
Bruno Alves <brunoacoelho86@gmail.com>
2021-03-25 13:33:16 -0300

    Projeto iniciado monstrado OLA MUNDO

---
+++ ConsoleAppProjetoTesteGit/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace ConsoleAppProjetoTesteGit
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.WriteLine();
+        }
+    }
+}
