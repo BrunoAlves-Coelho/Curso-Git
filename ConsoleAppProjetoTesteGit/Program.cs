beecef1553417633396f8364a26be118d8b2cc05
Bruno Alves <brunoacoelho86@gmail.com>
2021-03-25 16:57:15 -0300

    Acrescentando pergunta como esta sendo seu dia

--- ConsoleAppProjetoTesteGit/Program.cs
+++ ConsoleAppProjetoTesteGit/Program.cs
@@ -9,6 +9,7 @@ namespace ConsoleAppProjetoTesteGit
             Console.WriteLine("Olá mundo");
             Console.WriteLine("Boa tardE");
             Console.WriteLine("Como você está?");
+            Console.WriteLine("Com esta sendo seu dia?");
             Console.WriteLine();
         }
     }
